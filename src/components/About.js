c43707c2da7c7730d392c2254d4267a36a418642
kevin diesenberg <kdiesenb@gmail.com>
2022-11-22 09:24:58 -0500

    commit tree 7266dfee4721ea5d4ef84643b8df36751d5a4c5e

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return (
+      <footer>
+        <h2>About</h2>
+      </footer>
+    )
+}
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+// import NavBar from "./NavBar";
+// import Home from "./Home";
+// import About from "./About";
 
 function NavBar() {
   return (
@@ -16,17 +19,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
+
 
 export default App;
